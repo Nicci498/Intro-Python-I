e29e616500cfc1c85817620b0e957460aca41b3e
Nicole white <you@example.com>
2020-05-05 13:04:08 -1000

    14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,29 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+# with open('src/foo.txt') as file:
+#     print(file.read())
 
+from os import path
+absfile = path.join(path.dirname(path.abspath(__file__)), "foo.txt")
+with open(absfile, "r") as f:
+    print(f.read())
+f.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+from os import path
+absfile = path.join(path.dirname(path.abspath(__file__)), "bar.txt")
+with open(absfile, "w") as f:
+    content = "I am the new text"
+    print(f.write(content))
+f.close()
+
+from os import path
+absfile = path.join(path.dirname(path.abspath(__file__)), "bar.txt")
+with open(absfile, "r") as f:
+    print(f.read())
+f.close()
--- src/14_cal.py
+++ src/14_cal.py
@@ -30,3 +30,86 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+
+def new_calendar():
+  arg = input('Select a month, year and/or see this month').split()
+  yr = "20"
+  mth = "05"    
+
+  if len(arg) == 1:
+    mth = arg[0]
+
+  elif len(arg) == 2:
+    mth = ans[0]
+    yr = ans[1]
+    print(yr)
+  if mth.isdigit() == False or yr.isdigit() == False:
+    return print('please only enter numbers')
+  
+  year = int(yr)
+  month = int(mth)
+  print(year)
+
+  if month > 12 or month < 1:
+    return print('please enter a valid month')
+    
+  print(calendar.month(year, month))
+  print(f'month is {mth} and year is {yr}')
+  
+new_calendar()
+
+
+
+# current_date = datetime.now()
+# month = current_date.month
+# year = current_date.year
+
+# def change_month(new_month):
+#   global month
+#   month = new_month
+
+# def change_year(new_year):
+#   global year
+#   year = new_year
+
+# if len(sys.argv) == 2:
+#   change_month(int(sys.argv[1]))
+# elif len(sys.argv) == 3:
+#   change_month(int(sys.argv[1]))
+#   change_year(int(sys.argv[2]))
+# print(calendar.month(year, month))
+
+
+
+# date_and_time = datetime.now()
+# today = date_and_time.day
+# this_month = date_and_time.month
+# this_year = date_and_time.year
+# print(today, this_month, this_year)
+
+# def new_calendar(*args):
+#   if len(sys.argv) == 1:
+#     print(calendar.month(this_year, this_month))
+#   elif len(sys.argv) == 2:
+#     print(calendar.month(this_year, sys.argv[1]))
+#   elif len(sys.argv) == 3:
+#     print(calendar.month(sys.argv[2], sys.argv[1]))
+#   else:
+#     return 'a usage statement to the terminal indicating the format that your program expects arguments to be given'
+  
+   
+#nice formatted string of month print(calendar.month(2020, 5, w=3)) 
+# print(calendar.weekheader(3)) #prints mo-su arg=name.length
+# print(calendar.firstweekday()) # useful to have dates in number form
+# print(calendar.month(2020, 5, w=3)) #nice formatted string of month
+# print(calendar.monthcalendar(2020, 5)) #matrix (array of arrays of dates the weeks)
+# print(calendar.calendar(2020))
+# day_of_the_week = calendar.weekday(2020, 5, 6)
+# print(day_of_the_week)
+# is_leap = calendar.isleap(2020)
+# print(is_leap)
+# how_many_leap_days = calendar.leapdays(1990, 2021)#includes 90, but not 21
+# print(how_many_leap_days)
+
+
